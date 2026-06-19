e26fa0903fb9dcc62d805102133afc13c5510866
Your Name <your.email@example.com>
2026-06-19 15:07:08 +0530

    commit and push

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,18 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Welcome from './pages/Welcome'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Welcome from "./pages/Welcome";
 import Payment from "./pages/Payment";
-import PhoneForm from './pages/PhoneForm'
+import PhoneForm from "./pages/PhoneForm";
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
       <Route path="/" element={<Welcome />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/payment" element={<Payment />} />
-      <Route path="/phoneform" element={<PhoneForm/>}/>
+      <Route path="/phoneform" element={<PhoneForm />} />
+
+      {/* Fallback route */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
